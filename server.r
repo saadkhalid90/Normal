0b1207b05985abe6d7af4e805d4928e1a3511535
dorucojoc <cojocdoru@gmail.com>
2016-10-04 18:29:25 -0400

    Multiple types of questions
    - Randomizes between questions of the form P(X in interval), P(X>), and P(X<)
    
    Need to create questions on percentiles

--- server.r
+++ server.r
@@ -1,13 +1,16 @@
 library(shiny)
 
 sd_bound <- signif(sort(runif(n=2,min=-5,max=5)), 2)
+if(runif(1,0,1)<0.6) {
+  if(runif(1,0,1)<0.5) sd_bound[1]<--5 else sd_bound[2]<-5
+}
 mean <- round(runif(n = 1, min = 0, max = 100), digits = 1)
 # mean <- round(runif(n = 1, min = -100, max = 100), digits = 1) - Changed the range on the mean from -100 to 0
 sd <- round(runif(n = 1, min = 1, max = 20), digits = 0)
 # sd_bound <- signif(sort(runif(n = 2, min = -4, max = 4)), 3) - Replaced above to make sure the z score is in Table A
 bounds <- mean + (sd_bound * sd)
 
-normal_plot <- function(mean, sd, lb, ub) {
+normal_plot <- function(mean, sd, lb, ub, sd_bound) {
   ## finding the x and y coordinates for plotting
   x <- seq(-4,4,length=1000)*sd + mean
   hx <- dnorm(x,mean,sd)
@@ -15,17 +18,24 @@ normal_plot <- function(mean, sd, lb, ub) {
   ## plot
   plot(x, hx, type="n", xlab="X", ylab="", axes=FALSE)
   # plot(x, hx, type="n", xlab="R.V", ylab="", axes=FALSE) - Changed R.V. to X throughout
-
+  
   i <- x >= lb & x <= ub
   lines(x, hx)
   polygon(c(lb,x[i],ub), c(0,hx[i],0), col=rgb(1,0,0, alpha = 0.75)) 
   
   area <- pnorm(ub, mean, sd) - pnorm(lb, mean, sd)
-  # Changed the text above the graph a bit and removed the answer
   rv_text <- paste("X has Normal distribution")
   mean_txt <- paste("with Mean: ", mean)
+  # mean_txt <- paste("Mean: ", mean) - Changed the text above the graph a bit
   sd_text <- paste("and S.D: ", sd)
-  result <- paste(rv_text, "\n", mean_txt, "\n", sd_text, "\n", "What is P(",lb,"< X <",ub,")?")
+  if(sd_bound[1]==-5) {
+    result <- paste(rv_text, "\n", mean_txt, "\n", sd_text, "\n", "What is P(X <",ub,")?")  
+  }
+  else {
+    if(sd_bound[2]==5) result <- paste(rv_text, "\n", mean_txt, "\n", sd_text, "\n", "What is P(X >",lb,")?")
+    else result <- paste(rv_text, "\n", mean_txt, "\n", sd_text, "\n", "What is P(",lb,"< X <",ub,")?")
+  }
+  
   mtext(result,3)
   axis(1, at=seq(mean - (4 * sd), mean + ( 4 * sd), sd), pos=0)
 }
@@ -45,10 +55,13 @@ shinyServer(function(input, output, session){
                       incorrect = 0,
                       question_count = 0,
                       test_area = 0)
-
+  
   observeEvent(input$action, {
     # This is changed as above, at the beginning
     v$sd_bound <- signif(sort(runif(n=2, min=-5, max=5)), 2)
+    if(runif(1,0,1)<0.6) {
+      if(runif(1,0,1)<0.5) v$sd_bound[1]<--5 else v$sd_bound[2]<-5
+    }
     v$mean <- round(runif(n = 1, min = 0, max = 100), digits = 1)
     v$sd <- round(runif(n = 1, min = 1, max = 20), digits = 0)
     v$bounds <- v$mean + (v$sd_bound * v$sd)
@@ -75,8 +88,8 @@ shinyServer(function(input, output, session){
     }
     v$test_area <- area
   })
-
-  output$NormalPlot <- renderPlot(normal_plot(v$mean, v$sd, v$bounds[1], v$bounds[2]))
+  
+  output$NormalPlot <- renderPlot(normal_plot(v$mean, v$sd, v$bounds[1], v$bounds[2], v$sd_bound))
   output$ci <- renderText(v$cor_inc)
   output$cor <- renderText(paste("Correct: ", v$correct))
   output$incor <- renderText(paste("Incorrect: ", v$incorrect))
